2c3430495ca4fbaca6a0a8222eb21b8a7c9ae612
Sharon Raja <sharonrajad@gmail.com>
2025-04-05 09:23:21 +0530

    For loop programs to print patterns

---
+++ ForLoop.java
@@ -0,0 +1,98 @@
+// for loop 1 docx ans
+import java.util.Scanner;
+public class ForLoop {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // int i = 5;
+        // System.out.printf("%d %d %d", i++, i++, i++);
+    
+        
+        // System.out.println("*******1*********\nPrinting tables");
+        // System.out.print("Input the number (Table to be calculated) : ");
+        // int m = sc.nextInt();
+        // System.out.print("Input number of terms : ");
+        // int n = sc.nextInt();
+        // for(int i = 0; i<=n; i++){
+        //     System.out.println(m + " x " + i + " = " + (n*i) );
+        // }
+
+
+        // System.out.println("*******2*********\nSum of n odd natural numbers");
+        // System.out.print("Input number of terms is: ");
+        // int n = sc.nextInt();
+        // int sum = 0;
+        // System.out.println("The odd numbers are :");
+        // for(int i = 1; i < n*2; i+=2){
+        //     System.out.println(i);
+        //     sum += i;
+        // }
+        // System.out.println("The Sum of odd Natural Number upto " + n + " terms is: " + sum);
+
+        
+        // System.out.println("*******3*********\nRight angle triangle with a number");
+        // System.out.print("Input number of rows : ");
+        // int n = sc.nextInt();
+        // for(int i = 1; i<= n; i++){
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+
+
+        // System.out.println("*******4*********\nPyramid pattern with a number that repeats char");
+        // System.out.print("Input number of rows : ");
+        // int n = sc.nextInt();
+        // for(int i = 1; i<=n; i++){
+        //     // for(int s = n-1; s>=i;s--)
+        //     //     System.out.print(" ");
+            
+        //     // for(int j = 0; j<i; j++)
+        //     //     System.out.print(i + " ");
+
+        //     System.out.println(" ".repeat(n-i) + (i + " ").repeat(i) );
+        // }
+
+
+        // System.out.println("*******5*********\n Triangle using *");
+        // System.out.print("Input number of rows : ");
+        // int n = sc.nextInt();
+        // for(int i = n; i > 0; i--){
+        //     System.out.println(" ".repeat((n-i)*2)+ "* ".repeat(i));
+        // }
+
+
+        // System.out.println("*******6*********\n @'s triangle");
+        // System.out.print("Input number of rows : ");
+        // int n = sc.nextInt();
+        // for(int i = 1; i <=n; i++){
+        //     System.out.println(" ".repeat((n-i)*2)+ "@ ".repeat(i));
+
+        // }
+
+
+        // System.out.println("*******6*********\nFind the order of numbers");
+        // System.out.print("Input first number: ");
+        // long a = sc.nextLong();
+        // System.out.print("Input second number: ");
+        // long b = sc.nextLong();
+        // System.out.print("Input third number: ");
+        // long c = sc.nextLong();
+        // if(a<b && b<c){
+        //     System.out.println("Increasing order");
+        // }
+        // else if (a > b && b > c){
+        //     System.out.println("decreasing order");
+        // }
+        // else {
+        //     System.out.println("Neither increasing or decreasing order");
+        // }
+        
+
+        
+
+        /// main end
+    }
+}
