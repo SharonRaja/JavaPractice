1a99c4a0a684102ca6349196de8b212027a2c059
Sharon Raja <sharonrajad@gmail.com>
2025-04-07 16:49:58 +0530

    Few more pattern added

--- ForLoop2.java
+++ ForLoop2.java
@@ -312,19 +312,322 @@ class ForLoop2 {
                 *
     */
 
+        // System.out.print("Enter the number of row :");
+        // int n = sc.nextInt();
+        // for(int i =n; i>0;i--){
+        //     for(int j = n; j>0; j--){
+        //         if(j-i>=1){
+        //             System.out.print("  ");
+        //         }
+        //         else{
+        //             System.out.print(" *");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+    /* 16) K pattern
+        * * * *
+        * * *
+        * *
+        *
+        * *
+        * * *
+        * * * *
+
+    */
+        // System.out.print("Enter the number of row :");
+        // int n = sc.nextInt();
+        // for(int i =n; i>0;i--){
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("* ");
+               
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i =2; i<=n;i++){
+        //     for(int j = 1; j<=i; j++){
+        //         System.out.print("* ");
+               
+        //     }
+        //     System.out.println();
+        // }
+
+    /* 17) Triangle star pattern
+            *
+           * *
+          * * *
+         * * * *
+        * * * * *
+    */
+        // System.out.print("Enter the number of row :");
+        // int n = sc.nextInt();
+        // for(int i = 1; i<=n; i++){
+        //     for(int j=1; j<=n;j++){
+        //         if(j<=n-i){
+        //             System.out.print(" ");
+        //         }
+        //         else{
+        //             System.out.print("* ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+    /* 18) Reverse number triange pattern
+        1 2 3 4
+         2 3 4
+          3 4
+           4
+    */
+
+        // System.out.print("Enter the number of row :");
+        // int n = sc.nextInt();
+        // for(int i = 1; i<=n;i++){
+        //     for(int j = 1, x=i;j<=n;j++){
+        //         if(j<i){
+        //             System.out.print(" ");
+        //         }
+        //         else{
+        //             System.out.print(x++ + " ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+    /* 19) Mirror Image Triangle pattern
+        1 2 3 4
+         2 3 4
+          3 4
+           4
+          3 4
+         2 3 4
+        1 2 3 4
+    */
+
+        // System.out.print("Enter the number of row :");
+        // int n = sc.nextInt();
+        // for(int i = 1; i<=n;i++){
+        //     for(int j = 1, x=i;j<=n;j++){
+        //          if(j<i){
+        //             System.out.print(" ");
+        //          }
+        //          else{
+        //             System.out.print(x++ + " ");
+        //          }
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i = n-1; i>0;i--){
+        //     for(int j = 0, x=i;j<n;j++){
+        //         if(i>j+1){
+        //             System.out.print(" ");
+        //         }
+        //         else{
+        //             System.out.print(x++ + " ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+    /* 20) Hollow Triangle pattern
+            *
+           * *
+          *   *
+         *     *
+        * * * * *
+    */
+
+        // System.out.print("Enter the number of row :");
+        // int n = sc.nextInt();
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n - i - 1; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 0; j <i + 1; j++) {
+        //         if (j == 0 || j == i || i == n - 1) {
+        //             System.out.print("* "); 
+        //         } else {
+        //             System.out.print("  "); 
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+    /* 21) Hollow Triangle pattern
+        * * * * *
+         *     *
+          *   *
+           * *
+            * 
+    */
+        // System.out.print("Enter the number of row : ");
+        // int n = sc.nextInt();
+        // for (int i = n; i > 0; i--) {
+        //     for (int j = 0; j <= n - i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 0; j <= i - 1; j++) {
+
+        //         if (j == 0 || j == i - 1 || i == n) {
+        //             System.out.print("* ");
+        //         } else {
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+    /* 22) Hollow Triangle pattern
+            *
+           * *
+          *   *
+         *     *
+        *       *
+         *     *
+          *   *
+           * *
+            * 
+    */
+
+        // System.out.print("Enter the number of row :");
+        // int n = sc.nextInt();
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n - i - 1; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 0; j <i + 1; j++) {
+        //         if (j == 0 || j == i ) {
+        //             System.out.print("* "); 
+        //         } else {
+        //             System.out.print("  "); 
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = n-1; i > 0; i--) {
+        //     for (int j = 0; j <= n - i-1; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 0; j <= i - 1; j++) {
+
+        //         if (j == 0 || j == i - 1  ) {
+        //             System.out.print("* ");
+        //         } else {
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+    /* 23) Hollow Triangle pattern
+         * * * *
+          *   *
+           * *
+            *
+           * *
+          *   *
+         * * * * 
+    */
+        // System.out.print("Enter the number of row :");
+        // int n = sc.nextInt();
+        // for (int i = n; i > 0; i--) {
+        //     for (int j = 0; j <= n - i; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 0; j <= i - 1; j++) {
+
+        //         if (j == 0 || j == i - 1 || i == n) {
+        //             System.out.print("* ");
+        //         } else {
+        //             System.out.print("  ");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        // for (int i = 1; i < n; i++) {
+        //     for (int j = 0; j < n - i ; j++) {
+        //         System.out.print(" ");
+        //     }
+        //     for (int j = 0; j <i + 1; j++) {
+        //         if (j == 0 || j == i || i == n - 1) {
+        //             System.out.print("* "); 
+        //         } else {
+        //             System.out.print("  "); 
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+
+
+    /* 23) Pascal Triangle pattern
+            1
+           1 1
+          1 2 1
+         1 3 3 1
+        1 4 6 4 1 
+    */
+        // System.out.print("Enter the number of row :");
+        // int n = sc.nextInt();
+        // for (int row = 1; row <= n; row++) {
+        //     for (int j = 0; j <= n - row; j++) {
+        //         System.out.print(" ");
+        //     }
+ 
+        //     int C = 1; 
+        //     for (int i = 1; i <= row; i++) {
+        //         System.out.print(C + " "); 
+        //         C = C * (row - i) / i; 
+        //     }
+        //     System.out.println();
+        // }
+
+    /* 23) Hollow Triangle pattern
+         *
+           *
+         *   *
+           *   *
+         *   *
+           *
+         *  
+    */
+
         System.out.print("Enter the number of row :");
         int n = sc.nextInt();
-        for(int i =n; i>0;i--){
-            for(int j = n; j>0; j--){
-                if(j-i>=1){
-                    System.out.print("  ");
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= (i+1)/2; j++) {
+                if(i%2==0 ){
+                    System.out.print(" *"); 
                 }
-                else{
-                    System.out.print(" *");
+                else if(i%2!=0){
+                    System.out.print("* ");
                 }
+                else
+                    System.out.print(" ");
             }
             System.out.println();
         }
-    
+        System.out.println("----------");
+        for (int i = n; i >= 0; i--) {
+
+            for (int j = 0; j <= (i+1)/2; j++) {
+                if(i%2==0 ){
+                    System.out.print(" *");
+                    
+                }
+                else if(i%2!=0){
+                    System.out.print("* "); 
+                    
+                }
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
     }
 }
