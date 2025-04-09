f9052c12e1d108703c91d931172bd3b59bd5fb17
Sharon Raja <sharonrajad@gmail.com>
2025-04-09 10:40:51 +0530

    updated to print the amstrong series between 1 & 20

--- Amstrong.java
+++ Amstrong.java
@@ -3,9 +3,12 @@ import java.math.*;
 public class Amstrong {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number : ");
-        int c = 1,copy,n = sc.nextInt();
-        copy = n;
+        // System.out.print("Enter a number : ");
+        // int c = 1,copy,n = sc.nextInt();
+        int c = 1,copy, n;
+        for(int i =1; i<=20;i++){
+        copy = n = i ;
+
         while (n/10 >=1 ) {
             c++;
             n /= 10;
@@ -17,12 +20,13 @@ public class Amstrong {
             n =n/10;
         }
         sum += Math.pow(n%10,c);
-        System.out.println(sum);
+        // System.out.println(sum);
         if(sum == copy){
-            System.out.println("It is a amstron number");
+            System.out.println( i + " is a amstron number");
         }
         else{
-            System.out.println("It is not a amstrong number");
+            System.out.println(i + " is not a amstrong number");
         }
+    }
     }
 }
