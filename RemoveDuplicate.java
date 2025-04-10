2fc215c07b78361f32bb8eb5e7395b8d1cb8d9db
Sharon Raja <sharonrajad@gmail.com>
2025-04-10 12:04:20 +0530

    Remove duplicate bug fix to handle zero

--- RemoveDuplicate.java
+++ RemoveDuplicate.java
@@ -1,22 +1,27 @@
+import java.util.Arrays;
 public class RemoveDuplicate {
     public static void main(String[] args) {
-        int[] arr = {3,2,5,6, 8, 8,1, 0 , 0, 0};
-        int c = 0;
+        int[] arr = {3,2,5,6, 8, 8,1};
+        int count = 0;
+        boolean isDuplicate;
         int[] copy = new int[arr.length];
         for(int n : arr){
-            for(int i =0; i<arr.length ;i++){
+            // System.out.println(n);
+            isDuplicate = false;
+            for(int i =0; i<count ;i++){
                 if(n == copy[i]){
+                    isDuplicate = true;
                     break;
                 }
-                else if(i == arr.length-1){
-                    copy[c]=n;
-                    c++;
-                }
+            }
+            if(!isDuplicate){
+                // System.out.println(n);
+                copy[count++]=n;
             }
         }
-        System.out.print(" [ ");
-        for(int  x: copy){
-            System.out.print(x+", ");
+        System.out.print("[ ");
+        for(int i = 0;i<count;i++){
+            System.out.print(copy[i]+", ");
         }
         System.out.println("]");
  
