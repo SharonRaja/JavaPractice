3e123882cb9826510bfd102a67981049fd5c12c8
Sharon Raja <sharonrajad@gmail.com>
2025-04-10 11:30:12 +0530

    amstrong bug fix

--- Amstrong.java
+++ Amstrong.java
@@ -6,27 +6,30 @@ public class Amstrong {
         // System.out.print("Enter a number : ");
         // int c = 1,copy,n = sc.nextInt();
         int c = 1,copy, n;
-        for(int i =1; i<=20;i++){
-        copy = n = i ;
+        for(int i =1, count =0 ; count<=100 && i<=100000;i++){
+            c=1;
+            copy = n = i;
 
-        while (n/10 >=1 ) {
-            c++;
-            n /= 10;
-        }
-        int sum = 0;
-        n = copy;
-        while(n/10 >=1){
+            while (n/10 >=1 ) {
+                c++;
+                n /= 10;
+            }
+            int sum = 0;
+            n = copy;
+            while(n/10 >=1){
+                sum += Math.pow(n%10,c);
+                n =n/10;
+            }
             sum += Math.pow(n%10,c);
-            n =n/10;
+            // System.out.println(sum);
+            // System.out.println(i);
+            if(sum == copy){
+                System.out.println( i + " is a amstron number");
+                count++;
+            }
+            // else{
+            //     System.out.println(i + " is not a amstrong number");
+            // }
         }
-        sum += Math.pow(n%10,c);
-        // System.out.println(sum);
-        if(sum == copy){
-            System.out.println( i + " is a amstron number");
-        }
-        else{
-            System.out.println(i + " is not a amstrong number");
-        }
-    }
     }
 }
