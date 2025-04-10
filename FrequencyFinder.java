1c0056883c755bcc71f7b269dd652f12fd7b5366
Sharon Raja <sharonrajad@gmail.com>
2025-04-10 10:11:34 +0530

    Frequency finder added

--- FrequencyFinder.java
+++ FrequencyFinder.java
@@ -1,19 +1,47 @@
 public class FrequencyFinder {
-    public static void main(String[] args) {
+    // public static void main(String[] args) {
         
-        int[] arr = {3,2,5,6,1};
-        int c=0;
-        for(int n : arr){
-            for(int i=1;i<arr.length;i++){
-                if(n == arr[i]){
+    //     int[] arr = {3,2,5,6,1, 0 , 0, 0};
+    //     int c=0;
+    //     for(int i=0;i<arr.length;i++){
+    //         if(0 == arr[i]){
+    //             c++;
+    //             arr[i] = 0;
+    //         }
+    //     }
+    //     if(c>0)
+    //         System.out.print( "0 -> " + c);
+
+    //     for(int n : arr){
+    //         c = 0;
+    //         for(int i=0;i<arr.length;i++){
+    //             if(n == arr[i] && 0 != arr[i]){
+    //                 c++;
+    //                 arr[i] = 0;
+    //             }
+    //         }
+    //         if (c>0)
+    //             System.out.print(", " + n + " -> " + c);
+    //         c = 0;
+            
+    //     }  
+    // }
+
+    public static void main(String[] args) {
+        int[] arr = {3,2,5,6, 8, 8,1, 0 , 0, 0};
+        int c;
+        boolean[] isVisted = new boolean[arr.length];
+        for(int i = 0; i<arr.length; i++){
+            c = 1;
+            for(int j = i+1; j<arr.length; j++){
+                if (arr[i] == arr[j] && !isVisted[j] ){
                     c++;
-                    arr[i] = ;
+                    isVisted[j] = true;
                 }
             }
-    }
-
-        
-        
+            if(!isVisted[i])
+                System.out.print(arr[i] + " -> " + c + ", ");
+        }
     }
     
 }
