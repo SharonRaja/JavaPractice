b2df25faee99c0d84ec0d94d88fd4bbfcd92895b
Sharon Raja <sharonrajad@gmail.com>
2025-04-10 11:38:33 +0530

    Made changes to print series

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -2,18 +2,22 @@ import java.util.Scanner;
 public class PerfectNumber {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number :");
-        int sum = 1,n = sc.nextInt();
-        for(int i=2; i<=(n/2);  i++)
-            if(n%i == 0){
-                sum += i;
-            }
-        if(sum == n){
-            System.out.println("It is a perfect number");
-        } 
-        else{
-            System.out.println("It is not a perfect number");
+        // System.out.print("Enter a number :");
+        // int n = sc.nextInt(); 
+        for(int n = 0, count = 0; count<=10; n++){
+            int sum = 1;
+            for(int i=2; i<=(n/2);  i++)
+                if(n%i == 0){
+                    sum += i;
+                }
+            if(sum == n){
+                System.out.println(n + " is a perfect number");
+                count++;
+            } 
+            // else{
+            //     System.out.println("It is not a perfect number");
 
-        } 
+            // } 
+        }
     }
 }
