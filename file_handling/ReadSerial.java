b05e528d0764ef962680a72c0d8fbead34bbdb74
Sharon Raja <sharonrajad@gmail.com>
2025-04-22 15:15:02 +0530

    Added the code to write the data in the obj in txt file

--- file_handling/ReadSerial.java
+++ file_handling/ReadSerial.java
@@ -1,9 +1,11 @@
 package file_handling;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
 
 public class ReadSerial {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
@@ -13,6 +15,15 @@ public class ReadSerial {
         newobj.display();
         ois.close();
         fis.close();
+        PrintWriter pw = new PrintWriter(new File("Content.txt"));
+        pw.println("Car number : " + newobj.carNumber);
+        pw.println("Owner : " + newobj.ownerName);
+        pw.println("Brand : " + newobj.brand);
+        pw.println("Model : " + newobj.model);
+        pw.println("Year : " + newobj.year);
+        pw.println("Price : " + newobj.price);
+        pw.close();
+        System.out.println("Text file is created!!!");
         
 
     }
